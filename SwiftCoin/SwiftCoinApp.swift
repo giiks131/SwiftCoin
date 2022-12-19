98cc1008d90a39ff93e31cc16e2234d7bd357c8f
Alex Sh <giiks131@gmail.com>
2022-12-19 22:17:52 +0100

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Aleksandr Shapovalov on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
